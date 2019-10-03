37af39cd8f7fc383fb6d4e2b093400a66ba6e0dd
KateGJ <llanosvergara1997@gmail.com>
2019-10-03 14:24:33 +0200

    Create classes for the UC_buyProductos

---
+++ MikeRosoft/MikeRosoft/Models/Catalogue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikeRosoft.Models
+{
+    public class Catalogue
+    {
+        [Key]
+        public int idCatalogue;
+
+        public virtual IList<Product> products { get; set; }
+
+    }
+}
