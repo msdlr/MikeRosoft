d02bdd37a7ea17fb99b84bcfe80d9adeb649762a
ESTHER SANTOS MARTINEZ <Esther.Santos3@alu.uclm.es>
2019-10-03 14:24:39 +0200

    Class and attributes Recommendation

---
+++ MikeRosoft/MikeRosoft/Models/Recommendation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotation;
+using System.ComponentModel.DataAnnotation.Schema;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace MikeRosoft.Models {
+    public class Recommendation
+    {
+        [Key]
+        public virtual int ID { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public virtual DateTime date { get; set; }
+        
+        //Description about selected products
+        [Required]
+        [StringLength(180, MinimumLength = 1, ErrorMessage = "Description cannot be longer than 180 characters")]
+        public virtual string description { get; set;  }
+        
+        //Relacion con admin N-1
+        [ForeignKey("idamin")]
+        [Required]
+        public virtual string admin { get; set; }
+        
+        //Reasons about users
+        [Required]
+        [StringLength(180, MinimumLength = 1, ErrorMessage = "Reason cannot be longer than 180 characters")]
+        public virtual string reason { get; set; }
+        //Relacion con Usuarios N-N
+        //Relacion con Productos N-N
+
+        public Recommendation()
+        {
+
+        }
+    }
+}
