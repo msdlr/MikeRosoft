dff1855546a2111d5649ac3f1bd64539090327ff
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-10-06 23:36:46 +0200

    Added classes in Design for Ban User

---
+++ MikeRosoft/Design/Ban.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MikeRosoft.Design
+{
+    public class Ban
+    {
+        //Attributes
+        public virtual int ID { get; set; }
+
+        //Relationships
+        public virtual string GetAdminId { get; set; }
+
+        public virtual IList<BanForUser> GetBanForUsers { get; set; }
+        public virtual DateTime BanTime { get; set; }
+    }
+}
---
+++ MikeRosoft/Design/BanForUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MikeRosoft.Design
+{
+    public class BanForUser
+    {
+        //Attributes
+        public virtual int ID { get; set; }
+        public virtual string AdditionalComment { get; set; }
+        public virtual DateTime Start { get; set; }
+        public virtual DateTime End { get; set; }
+
+        //Relationships
+        public int GetBanID { get; set; }
+        
+        public string GetUserId { get; set; }
+        
+        public string GetBanTypeName { get; set; }
+    }
+}
