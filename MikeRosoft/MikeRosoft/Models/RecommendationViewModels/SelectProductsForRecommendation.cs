ed23d68786b5bf0a1b27b0da9139945c62833926
ESTHER SANTOS MARTINEZ <Esther.Santos3@alu.uclm.es>
2019-10-18 19:30:21 +0200

    Created SelectProductsForRecomendation, RecommendationsController and views

---
+++ MikeRosoft/MikeRosoft/Models/RecommendationViewModels/SelectProductsForRecommendation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikeRosoft.Models.RecommendationViewModels
+{
+    public class SelectProductsForRecommendation
+    {
+        public IEnumerable<Product> products { get; set; }
+
+        //filtro
+        //public SelectList nombredelfiltro
+        //...
+        
+        [Display(Name = "title")]
+        public string productTitleSelected { get; set; }
+
+        public SelectList brand;
+        [Display (Name = "brand")]
+        public string productBrandSelected { get; set; }
+
+        [Display (Name = "precio")]
+        public string productPrecioSelected { get; set; }
+
+        //Atributos a mostrar cuando hacemos el filtro
+        //[Display(name = "nombreatributo")]
+        //public tipo productoatributoseleccionado {get; set;}
+        //...
+    }
+}
