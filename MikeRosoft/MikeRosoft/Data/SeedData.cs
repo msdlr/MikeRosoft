0845c0783b71f811ff31eba7154582037edc1bbe
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-10-24 21:05:35 +0200

    Enabled SeedData

---
+++ MikeRosoft/MikeRosoft/Data/SeedData.cs
@@ -0,0 +1,112 @@
+﻿using MikeRosoft.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MikeRosoft.Data;
+
+namespace AppForMovies.Data
+{
+    public static class SeedData
+    {
+        //public static void Initialize(UserManager<ApplicationUser> userManager,
+        //            RoleManager<IdentityRole> roleManager)
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var role = serviceProvider.GetRequiredService(typeof(RoleManager<IdentityRole>));
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+
+            List<string> rolesNames = new List<string> { "Admin", "User" };
+
+            SeedRoles(roleManager, rolesNames);
+            SeedUsers(userManager, rolesNames);
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager, List<string> roles)
+        {
+
+            foreach (string roleName in roles)
+            {
+                //it checks such role does not exist in the database 
+                if (!roleManager.RoleExistsAsync(roleName).Result)
+                {
+                    IdentityRole role = new IdentityRole();
+                    role.Name = roleName;
+                    role.NormalizedName = roleName;
+                    IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+                }
+            }
+
+        }
+
+        public static void SeedUsers(UserManager<IdentityUser> userManager, List<string> roles)
+        {
+            //first, it checks the user does not already exist in the DB
+            if (userManager.FindByNameAsync("ms@uclm.es").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "ms@uclm.es";
+                user.Email = "ms@uclm.es";
+                user.Name = "Miguel";
+                user.FirstSurname = "Sanchez";
+                user.SecondSurname = "De la Rosa";
+
+                IdentityResult result = userManager.CreateAsync(user, "Password1234%").Result;
+
+                if (result.Succeeded)
+                {
+                    //administrator role
+                    userManager.AddToRoleAsync(user, roles[0]).Wait();
+                    user.EmailConfirmed = true;
+                }
+            }
+
+            if (userManager.FindByNameAsync("namesurname@uclm.com").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "namesurname@uclm.com";
+                user.Email = "namesurname@uclm.com";
+                user.Name = "Gregorio";
+                user.FirstSurname = "Diaz";
+                user.SecondSurname = "Descalzo";
+
+                IdentityResult result = userManager.CreateAsync(user, "APassword1234%").Result;
+
+                if (result.Succeeded)
+                {
+                    //Employee role
+                    userManager.AddToRoleAsync(user, roles[1]).Wait();
+                    user.EmailConfirmed = true;
+                }
+            }
+
+            if (userManager.FindByNameAsync("namesurname@uclm.com").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "namesurname@uclm.com";
+                user.Email = "namesurname@uclm.com";
+                user.Name = "Name";
+                user.FirstSurname = "Surname";
+                user.SecondSurname = "SurSurname";
+
+                IdentityResult result = userManager.CreateAsync(user, "APassword1234%").Result;
+
+                if (result.Succeeded)
+                {
+                    //Employee role
+                    userManager.AddToRoleAsync(user, roles[1]).Wait();
+                    user.EmailConfirmed = true;
+                }
+            }
+        }
+    }
+}
+
+
--- MikeRosoft/MikeRosoft/Program.cs
+++ MikeRosoft/MikeRosoft/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppForMovies.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MikeRosoft.Data;
 
 namespace MikeRosoft
 {
@@ -13,7 +16,25 @@ namespace MikeRosoft
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+
+                try
+                {
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
