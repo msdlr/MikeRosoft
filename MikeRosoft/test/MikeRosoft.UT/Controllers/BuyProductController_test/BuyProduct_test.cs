b4bb30efbc05f48a5eec237abe05e11396067ea8
KateGJ <llanosvergara1997@gmail.com>
2019-11-09 11:21:56 +0100

    BuyProduct controller test

---
+++ MikeRosoft/test/MikeRosoft.UT/Controllers/BuyProductController_test/BuyProduct_test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MikeRosoft.Data;
+using MikeRosoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
