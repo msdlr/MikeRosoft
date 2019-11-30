23a18a77becf154b72d2fb88fe8fb729c29efd8c
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-11-30 22:57:30 +0100

    Uploaded SQL scripts to add the Ban types and the admin to test the app, and the users

---
+++ MikeRosoft/MikeRosoft/Data/SQL/Bans/BanTypes_Admin.sql
@@ -0,0 +1,23 @@
+﻿/* Ban types */
+
+SET IDENTITY_INSERT [dbo].[BanTypes] ON
+INSERT INTO [dbo].[BanTypes] ([TypeID], [TypeName], [DefaultDuration]) VALUES (1, N'Griefing', N'12:00:00')
+SET IDENTITY_INSERT [dbo].[BanTypes] OFF
+
+SET IDENTITY_INSERT [dbo].[BanTypes] ON
+INSERT INTO [dbo].[BanTypes] ([TypeID], [TypeName], [DefaultDuration]) VALUES (2, N'Inappropiate comment', N'23:59:59')
+SET IDENTITY_INSERT [dbo].[BanTypes] OFF
+
+
+SET IDENTITY_INSERT [dbo].[BanTypes] ON
+INSERT INTO [dbo].[BanTypes] ([TypeID], [TypeName], [DefaultDuration]) VALUES (3, N'Unpaid orders', N'23:59:59')
+SET IDENTITY_INSERT [dbo].[BanTypes] OFF
+
+
+SET IDENTITY_INSERT [dbo].[BanTypes] ON
+INSERT INTO [dbo].[BanTypes] ([TypeID], [TypeName], [DefaultDuration]) VALUES (4, N'Fraudulent information', N'23:59:59')
+SET IDENTITY_INSERT [dbo].[BanTypes] OFF
+
+
+/* Admin */
+INSERT INTO [dbo].[AspNetUsers] ([Id], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount], [Discriminator], [Name], [FirstSurname], [SecondSurname], [DNI], [contractStarting], [contractEnding], [Street], [City], [Province], [Country]) VALUES (N'79df0829-1bb5-4972-8cb6-9d23b9aa59aa', N'ms@uclm.es', N'MS@UCLM.ES', N'ms@uclm.es', N'MS@UCLM.ES', 1, N'AQAAAAEAACcQAAAAELZt4jiv4PPZF2NWQrU8BPvXjYEBcOo6F62I1KlgHZ5+lybw6MyZixxsrlLuqvuboQ==', N'U7GFJM6MLSTI3LVVZ2CEUJZWQQKGDPWH', N'2401b374-4654-4618-a028-571d49fec2fb', NULL, 0, 0, NULL, 1, 0, N'Admin', N'Miguel', N'Sanchez', N'De la Rosa', NULL, N'0001-01-01 00:00:00', N'0001-01-01 00:00:00', NULL, NULL, NULL, NULL)
