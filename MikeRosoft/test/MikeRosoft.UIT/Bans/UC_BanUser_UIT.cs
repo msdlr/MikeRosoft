cdd1e4acdb059fb140dde97b4601fa8352dc5d46
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-12-18 17:44:27 +0100

    Created functions for UIT (still no code)

--- MikeRosoft/test/MikeRosoft.UIT/Bans/UC_BanUser_UIT.cs
+++ MikeRosoft/test/MikeRosoft.UIT/Bans/UC_BanUser_UIT.cs
@@ -12,7 +12,7 @@ using System.Text;
 using System.Threading;
 using Xunit;
 
-namespace MikeRosoft.UIT.Products
+namespace MikeRosoft.UIT.Bans
 {
     public class UC_BanUser_UIT : IDisposable
     {
@@ -51,6 +51,8 @@ namespace MikeRosoft.UIT.Products
             initial_step_opening_the_web_page();
         }
 
+        /* Testing of the pre-condition */
+
         [Fact]
         public void initial_step_opening_the_web_page()
         {
@@ -70,7 +72,6 @@ namespace MikeRosoft.UIT.Products
         [Fact]
         public void precondition_perform_login()
         {
-
             _driver.Navigate().GoToUrl(_URI + "Identity/Account/Login");
             _driver.FindElement(By.Id("Input_Email")).SendKeys("ms@uclm.es");
             _driver.FindElement(By.Id("Input_Password")).SendKeys("Password1234%");
@@ -81,60 +82,24 @@ namespace MikeRosoft.UIT.Products
             Assert.Equal(expectedTitle, _driver.Title);
         }
 
+        /* Scenario 1 - Main Flow, Successful ban */
         [Fact]
-        public void selectBanUserFromIndex()
+        public void UC1_30_1() //Successful ban
         {
-            //Go to the page, login and go to SelectUsersToBan
-            _driver.Navigate().GoToUrl(_URI);
-            precondition_perform_login();
-            _driver.FindElement(By.Id("BansLink")).Click();
-
-            string expectedTitle = "Select User(s) to ban - MikeRosoft";
-            Assert.Equal(expectedTitle, _driver.Title);
-        }
-        private void logout()
-        {
-            //Go to the page, login and go to SelectUsersToBan
-            _driver.Navigate().GoToUrl(_URI + "/Identity/Account/Logout?returnUrl=%2F");
 
         }
 
+        /* Scenario 2 - No users in the database */
         [Fact]
-        public void select_ClickNoFilter()
+        public void UC1_30_2() //No users in db
         {
-            //Open page, login and go to Select webpage
-            this.initial_step_opening_the_web_page();
-            this.precondition_perform_login();
-            this.selectBanUserFromIndex();
 
-            //Check that we correctly get to select users
-            Assert.Equal("Select User(s) to ban - MikeRosoft", _driver.Title);
-
-            string[] expectedText = { "48484848B", "Elena", "Navarro", "Martínez" };
-
-            var row = _driver.FindElements(By.Id("User_48484848B"));
-            Assert.NotNull(row);
-
-            //checks every column has those data expected
-            foreach (string expected in expectedText)
-                Assert.NotNull(row.First(l => l.Text.Contains(expected)));
-
-
-            //Click on the filter button
-            _driver.FindElement(By.Id("FilterButton")).Click();
-
-            var rowAfterFilter = _driver.FindElements(By.Id("User_48484848B"));
-            Assert.NotNull(row);
-
-            //checks every column has those data expected
-            foreach (string expected in expectedText)
-                Assert.NotNull(rowAfterFilter.First(l => l.Text.Contains(expected)));
-
-            this.logout();
         }
 
+        /* Scenario 3 - Filters */
+
         [Fact]
-        public void select_ClickFilterByName()
+        public void UC1_30_3() //Filter by name
         {
             //Open page, login and go to Select webpage
             this.initial_step_opening_the_web_page();
@@ -154,16 +119,13 @@ namespace MikeRosoft.UIT.Products
                 Assert.NotNull(row.First(l => l.Text.Contains(expected)));
 
             //Write the name in the filter
+            this.selectUsersToBan_filter("A", null, null, null);
 
-
-
-            //Click on the filter button
-            _driver.FindElement(By.Id("FilterButton")).Click();
-
-            var rowAfterFilter = _driver.FindElements(By.Id("User_48484848B"));
+            //Check the one wit that name is displayed
+            var rowAfterFilter = _driver.FindElements(By.Id("User_"+expectedText[0]));
             Assert.NotNull(row);
 
-            //checks every column has those data expected
+            //checks the row of the table
             foreach (string expected in expectedText)
                 Assert.NotNull(rowAfterFilter.First(l => l.Text.Contains(expected)));
 
@@ -171,72 +133,72 @@ namespace MikeRosoft.UIT.Products
         }
 
         [Fact]
-        public void select_ClickFilter1stSurname()
+        public void UC1_30_4() //Filter by 1st surname
         {
 
         }
 
         [Fact]
-        public void select_ClickFilter2ndSurname()
+        public void UC1_30_5() //Filter by 2nd surname 
         {
 
         }
 
         [Fact]
-        public void select_ClickFilterDNI()
+        public void UC1_30_6() //Filter by DNI
+        {
+
+        }
+
+        /* Scenario 3 - AF2, no users selected */
+
+        [Fact]
+        public void UC1_30_7() //No users selected
+        {
+
+        }
+
+        /* Scenario 3 - Missing mandatory data in create view */
+
+        [Fact]
+        public void UC1_30_8() //No ban type selected
         {
 
         }
 
         [Fact]
-        public void create_NoDataInput()
+        public void UC1_30_9() //Start date not selected
         {
 
         }
 
         [Fact]
-        public void create_MissingReason()
+        public void UC1_30_10() //End date not selected
         {
 
         }
 
         [Fact]
-        public void create_MissingStartDate()
+        public void UC1_30_11() //Start date < Today
         {
 
         }
 
         [Fact]
-        public void create_MissingEndDate()
+        public void UC1_30_12() //End date > Start date
         {
 
         }
 
-        [Fact]
-        public void create_withComment()
+
+        private void logout()
         {
+            //Go to the page, login and go to SelectUsersToBan
+            _driver.Navigate().GoToUrl(_URI + "/Identity/Account/Logout?returnUrl=%2F");
 
         }
 
-        [Fact]
-        public void create_withoutComment()
-        {
-
-        }
-
-        [Fact]
-        public void details_Null()
-        {
-
-        }
-
-        [Fact]
-        public void details_NotNull()
-        {
-
-        }
-
-        private void select_filter(string NameSelected, string userSurname1, string userSurname2, string userDNI)
+        private void selectUsersToBan_filter(string NameSelected, string userSurname1, string userSurname2, string userDNI)
         {
             if (NameSelected != null)
             {
@@ -261,6 +223,36 @@ namespace MikeRosoft.UIT.Products
             {
                 _driver.FindElement(By.Id("userDNI")).SendKeys(userDNI);
                 _driver.FindElement(By.Id("FilterButton")).Click();
+            }
+
+            //Click on the filter button
+            _driver.FindElement(By.Id("FilterButton")).Click();
+        }
+
+        private void selectBanUserFromIndex()
+        {
+            //Go to the page, login and go to SelectUsersToBan
+            _driver.Navigate().GoToUrl(_URI);
+            precondition_perform_login();
+            _driver.FindElement(By.Id("BansLink")).Click();
+
+            string expectedTitle = "Select User(s) to ban - MikeRosoft";
+            Assert.Equal(expectedTitle, _driver.Title);
+
+            //This are the users expected on the database
+            List<string[]> expectedUserList = new List<string[]>
+            {
+                new string[]{ "48484848B" ,"Elena","Navarro","Martínez" },
+                new string[]{ "48484848C","Name","Surname","SurSurname" },
+                new string[]{ "12345678J", "A","B","C"}
+            };
+
+            //Check that these users appear in the table
+            foreach (var row in expectedUserList)
+            {
+                var r = _driver.FindElements(By.Id("User_" + row[0]));
+                //Check that it is not null
+                Assert.NotNull(r);
             }
         }
 
