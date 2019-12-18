fc20204dba3eb7f19c60cdf271159191050ed859
Miguel Sánchez <miguelsrosa97@gmail.com>
2019-12-18 18:13:40 +0100

    Added SQL script to delete all users from the db other than ms

---
+++ MikeRosoft/test/MikeRosoft.UT/Controllers/BansControllers_test/SQL/DeleteUsers.sql
@@ -0,0 +1,6 @@
+﻿/* Delete roles */
+
+Delete from [dbo].[AspNetUserRoles] where [RoleId] != N'6d71dd01-8c6e-473c-8313-366e3428e9a2'
+
+/* Delete other users */
+Delete from [dbo].[AspNetUsers] where UserName != 'ms@uclm.es'
